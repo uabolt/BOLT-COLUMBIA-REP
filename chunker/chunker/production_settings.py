1802bb55ab01c71b0709f4913d3657acd58f7ebc
Enrique Noriega <enoriega@dalek.sista.arizona.edu>
2014-06-09 17:37:36 -0700

    Modified production settings for presto

--- chunker/chunker/production_settings.py
+++ chunker/chunker/production_settings.py
@@ -13,12 +13,17 @@ ALLOWED_HOSTS = ['localhost', 'uabolt.sista.arizona.edu']
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    },
+
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'uabolt',
         'USER': 'enoriega',
         'PASSWORD': 'w7PxNoAojIWsDA',
-        'HOST': 'presto',   # Or an IP Address that your DB is hosted on
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
 }
