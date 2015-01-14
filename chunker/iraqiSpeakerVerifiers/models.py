8b3ef962540e1f7918a92186c4b23dd59e3d0e66
David Sidi <git.spamhungry@spamgourmet.net>
2015-01-13 22:18:43 -0700

    fixed translation to arabic problem

--- chunker/iraqiSpeakerVerifiers/models.py
+++ chunker/iraqiSpeakerVerifiers/models.py
@@ -7,7 +7,7 @@ import uuid
 class SpeakerVerification(models.Model):
     user_code = models.CharField(max_length=32, default=lambda:uuid.uuid4().hex)
 
-    answer1 = models.CharField(max_length=200, verbose_name = _('baghdad is divided into two major halves. What are they?'))
+    answer1 = models.CharField(max_length=200, verbose_name = _('Baghdad is divided into two major halves. What are they?'))
 
     answer2 = models.CharField(verbose_name = _("what are the common nicknames for the following Iraqi Names (Nickname starts with Abu): Ali,Hussein and Mohammed?"), max_length=200)
 
