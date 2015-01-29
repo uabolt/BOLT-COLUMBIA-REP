7c9f8c7c97151f07a9efc1b1a257add5e2b0c322
enoriega <enrique.noriega@gmail.com>
2015-01-29 16:56:40 -0700

    Configuration change
    Change to the wsgi file to reflect the new configuration file name

--- chunker/b4_wsgi.py
+++ chunker/b4_wsgi.py
@@ -1,12 +1,7 @@
 import os
-import sys      
-<<<<<<< HEAD
+import sys
 sys.path.insert(0, '/var/www/virtualenvs/MT-UABOLT/lib/python2.7/site-packages')
 sys.path.append('/var/www/mt-uabolt/chunker')
-=======
-sys.path.insert(0, '/var/www/virtualenvs/UA-BOLT/lib/python2.7/site-packages')
-sys.path.append('/var/www/uabolt/chunker')
->>>>>>> master
-os.environ['DJANGO_SETTINGS_MODULE'] = 'chunker.b4_settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'chunker.b4_amt_settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
