48d2619731c9b3bb373afa34d70d339c873a3eef
enoriega <enrique.noriega@gmail.com>
2015-01-30 17:59:38 -0700

    First unit tests for dataset assignment defined

--- chunker/interface/middleware/dataset.py
+++ chunker/interface/middleware/dataset.py
@@ -3,28 +3,29 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from os.path import basename, splitext
 from interface.models import DatasetAssignment as DSA
-import glob, random, os, shutil, re, pdb
+from interface.models import *
+import glob, random, os, shutil, re, pdb, chunker
 
 pattern = re.compile(r'^/admin/?')
 
 class AssignDataset:
-    
+
     def process_request(self, request):
         ''' Assigns a dataset to the session if it doesn't exist yet. This middleware should go after the session middleware'''
-        
+
         #if not pattern.match(request.path) and not reverse('finish') in request.path and not reverse('instructions') in request.path:
         if reverse('pos_annotation') in request.path or reverse('reph_annotation') in request.path:
             path = settings.DATASET_DIR
-        
+
             # Dataset should be a non empty sequence on the
             if not 'dataset' in request.session or len(request.session['dataset']) == 0:
-            
+
                 # Retrieve the dataset files
                 files = glob.glob(os.path.join(path, "*.ds"))
-                
+
                 if len(files) == 0:
                     return HttpResponseRedirect(reverse('finish'))
-            
+
                 l = 1
                 while l != 0:
                     # Generate a random index
@@ -33,14 +34,14 @@ class AssignDataset:
                     # Here we check if this dataset has been previously assigned to the same session_id
                     dsname = basename(files[ix]).split('.')[0]
                     qs = DSA.objects.filter(session_id = request.session.session_key).filter(file_prefix__startswith = dsname[:-2])
-                    
+
                     l = len(qs)
 
                 obj = DSA(session_id = request.session.session_key, file_prefix= dsname)
                 obj.save()
 
 
-            
+
                 # Read the contents
                 with open(files[ix], 'r') as f:
                     fields = ('id', 'ref', 'chunked', 'segmented', 'control')
@@ -50,15 +51,47 @@ class AssignDataset:
                         sentences.append(dict(zip(fields, line.split('\t'))))
                 request.session['dataset'] = sentences
                 request.session['ds_file'] = files[ix].split('/')[-1] # Last token, which is the actual file name
-            
+
                 # Move the file to the processed files folder
                 path = os.path.join(path, 'processed')
                 if not os.path.exists(path):
                     os.makedirs(path)
-            
+
                 shutil.move(files[ix], path)
-        
-        
+
+
         return None
-        
-        
+
+
+# Assignment functions
+def assign_sentence(user_id):
+    '''
+        Gets a user_id, which is its session key, and returns an instance of DataItem
+
+        The dataset item shouldn't have been seen by the same user before and it
+        should be comprenhensive, meaning that before showing the same dataset item
+        to another user, all the elements of the dataset should be annotated by someone
+        for the current round, unless the remaining elements have already been seen by the
+        current user.
+    '''
+
+    raise NotImplementedError('This is not implemented yet')
+
+def remaining_sentences_in_task(user_id):
+    '''
+        Returns the number of remaining elements needed to complete a task.
+
+        Its value should be within 0 and chunker.settings.TASK_SIZE
+    '''
+
+    return chunker.settings.TASK_SIZE - AnnotationRecord.objects.filter(annotator = user_id).count()
+
+class DatasetExhaustedException(Exception):
+    ''' Exception raised if the dataset has been exhausted
+
+        The exhaustion is defined when all the elements have been annotated the number
+        of times defined in chunker.settings.MAX_ANNOTATIONS or when the remaining elements
+        are not suitable to be annotated by the current user
+
+    '''
+    pass
--- chunker/interface/tests.py
+++ chunker/interface/tests.py
@@ -1,3 +1,77 @@
 from django.test import TestCase
+import chunker
+from interface.middleware.dataset import *
+from interface.models import DataItem, AnnotationRecord
 
 # Create your tests here.
+class DataSetAssignmentTests(TestCase):
+    fixtures = ['dataset_test']
+
+    user1 = 'u1'
+    user2 = 'u2'
+
+    @classmethod
+    def setUpClass(self):
+        ''' Setup code for the test case '''
+
+        # Set the maximum number of annotations to 2
+        chunker.settings.MAX_ANNOTATIONS = 2
+        chunker.settings.TASK_SIZE = 3 # This is for convenience as well
+
+        # Populate some assignments
+        self.i1 = DataItem.objects.get(ref_id='evalTranstac-0702-fieldStructured1-120')
+        self.i2 = DataItem.objects.get(ref_id='evalTranstac-0702-fieldStructured1-128')
+        self.i3 = DataItem.objects.get(ref_id='evalTranstac-0702-fieldStructured1-130')
+
+        r1, r2 = AnnotationRecord(), AnnotationRecord()
+
+        r1.item = self.i1
+        r1.annotator = self.user1
+        r2.item = self.i2
+        r2.annotator = self.user2
+
+
+        r1.save()
+        r2.save()
+
+    def test_task_size(self):
+        ''' This tests the number of remaining sentences in a task '''
+
+        num = AnnotationRecord.objects.filter(annotator = self.user1).count()
+
+        self.assertEqual(remaining_sentences_in_task(self.user1), chunker.settings.TASK_SIZE - num) # Because in here the task size is 3
+
+    def test_get_remaining_element(self):
+        ''' This test checks that the comprenhensive feature is enforced '''
+
+        item = assign_sentence(self.user1)
+
+        self.assertEqual(item.id, 3)
+
+    def test_number_of_records(self):
+        ''' Checks if the AnnotationRecord instances is updated properly '''
+
+        self.assertEqual(AnnotationRecord.objects.count(), 3)
+
+    def test_respect_limits(self):
+        ''' Checks that the system doesn't allow more annotations '''
+
+        # Make some fake records
+        for i in range(2):
+            r = AnnotationRecord()
+            r.item = self.i1
+            r.annotator = self.user1
+            r.save()
+
+            r = AnnotationRecord()
+            r.item = self.i2
+            r.annotator = self.user1
+            r.save()
+
+            r = AnnotationRecord()
+            r.item = self.i3
+            r.annotator = self.user1
+            r.save()
+
+        # Now, there should be an exception
+        self.assertRaises(DatasetExhaustedException, assign_sentence, self.user2)
