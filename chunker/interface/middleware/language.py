50ab8907849edee9a78647fcb42bd2a201ce7c39
enoriega <enrique.noriega@gmail.com>
2015-03-26 16:01:04 -0700

    Fixed a translation bug
    Whenever someone access the admin site, the language got stuck to
    english locally. This has been fixed

--- chunker/interface/middleware/language.py
+++ chunker/interface/middleware/language.py
@@ -4,8 +4,19 @@ from django.utils import translation
 
 class AdminLocaleURLMiddleware:
 
+    original_lang = None
+
+    def __init__(self):
+        self.original_lang = getattr(settings, 'LANGUAGE_CODE', settings.LANGUAGE_CODE)
+
     def process_request(self, request):
         if request.path.startswith('/admin'):
             request.LANG = getattr(settings, 'ADMIN_LANGUAGE_CODE', settings.LANGUAGE_CODE)
             translation.activate(request.LANG)
             request.LANGUAGE_CODE = request.LANG
+
+    def process_response(self, request, response):
+
+        translation.activate(self.original_lang)
+
+        return response
