ddfae0670915d90a6882e8d84be8ed0491ab2c83
enoriega <enrique.noriega@gmail.com>
2015-04-01 14:42:13 -0700

    Finished implementing the catalog app functionality

--- chunker/catalogs/urls.py
+++ chunker/catalogs/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     url(r'^([a-fA-F0-9]{32})/pos/?$', POSListView.as_view(), name="hit-pos-list"),
     url(r'^([a-fA-F0-9]{32})/reph/?$', RephListView.as_view(), name="hit-reph-list"),
     url(r'^pos/(?P<pk>\d+)/?$', POSDetailView.as_view(), name="hit-pos-detail"),
-    url(r'^reph/(?P<pk>\d+)/\n+/?$', RephDetailView.as_view(), name="hit-reph-detail"),
+    url(r'^reph/(?P<pk>\d+)/?$', RephDetailView.as_view(), name="hit-reph-detail"),
 )
--- chunker/catalogs/views.py
+++ chunker/catalogs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.base import ContextMixin
 from django.views.generic import ListView, DetailView, TemplateView
+from django.forms import ModelForm
 from interface.models import POSAnnotation, RephAnnotation
 from iraqiSpeakerVerifiers.models import SpeakerVerification
 
@@ -14,22 +16,10 @@ class InterfaceListView(ListView):
 
         return model_class.objects.filter(user_code = self.user_code)
 
-
-
-class POSListView(InterfaceListView):
-    model = POSAnnotation
-
-class RephListView(InterfaceListView):
-    model = RephAnnotation
-
-class POSDetailView(DetailView):
-    model = POSAnnotation
-
-class RephDetailView(DetailView):
-    model = RephAnnotation
-
 class HITListView(TemplateView):
     ''' This is not a ListView but rather displays a list of the HITs '''
+
+    template_name = 'hit_list.html'
 
     def get_context_data(self, **kwargs):
         ''' Overriding the method '''
@@ -45,3 +35,62 @@ class HITListView(TemplateView):
 
         # Finish
         return context
+
+class ObjectTypeMixin(ContextMixin):
+
+    def get_context_data(self, **kwargs):
+        ''' This is to add parameters without overriding this method in the views '''
+
+        context = super(ObjectTypeMixin, self).get_context_data(**kwargs)
+        context['obj_type'] = self.obj_type
+
+        if hasattr(self, 'detail_url'):
+            context['detail_url'] = self.detail_url
+
+        return context
+
+class POSListView(InterfaceListView, ObjectTypeMixin):
+
+    template_name = 'catalog_list.html'
+    obj_type = "POS annotations"
+    model = POSAnnotation
+    detail_url = 'hit-pos-detail'
+
+class RephListView(InterfaceListView, ObjectTypeMixin):
+
+    template_name = 'catalog_list.html'
+    obj_type = "Reph annotations"
+    model = RephAnnotation
+    detail_url = 'hit-reph-detail'
+
+class POSDetailView(DetailView, ObjectTypeMixin):
+
+    template_name = 'catalog_detail.html'
+    obj_type = "POS annotation"
+    model = POSAnnotation
+
+    class POSForm(ModelForm):
+        class Meta:
+            model = POSAnnotation
+
+    def get_object(self):
+        entity = super(POSDetailView, self).get_object()
+        form = self.POSForm(instance=entity)
+
+        return form
+
+class RephDetailView(DetailView, ObjectTypeMixin):
+
+    template_name = 'catalog_detail.html'
+    obj_type = "Reph annotation"
+    model = RephAnnotation
+
+    class RephForm(ModelForm):
+        class Meta:
+            model = RephAnnotation
+
+    def get_object(self):
+        entity = super(RephDetailView, self).get_object()
+        form = self.RephForm(instance=entity)
+
+        return form
